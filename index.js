e3a54a9a089fa99822eb9b97324bd64e49b1aa81
sbal13 <sbal13@gmail.com>
2017-08-08 20:09:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+function bumpCounter(){
+	let counter = 0
+
+	function addBump(){
+		counter ++
+	}
+
+	function getBumps(){
+		return counter
+	}
+
+	return {addBump, getBumps}
+}
+
+
+
+function createAnimal(animalType){
+	function deadlyDevice(device){
+		return {animalType: animalType, deadlyDevice: device}
+	}
+
+	return deadlyDevice
+}
+
+var sharkCreator = createAnimal("Shark")
+
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+
+var sharkWithFrickinCannon = sharkCreator("Cannon")
+
